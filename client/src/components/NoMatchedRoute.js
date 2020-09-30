34183dcadc85dda3595f5f9d845afc5299928db5
Alexander Mikhalchenko <alex@xfuturum.com>
2020-09-30 04:33:37 +0300

    Initial commit of the frontend
    (cherry picked from commit 2ce8447e3983e9f4b45fcc12b8c4e56f299fae01)

---
+++ client/src/components/NoMatchedRoute.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NoMatchedRoute = () => (
+  <>
+    <h1>Oooof</h1>
+    <p>Not found</p>
+  </>
+);
+
+export default NoMatchedRoute;
---
+++ client/src/components/Register.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Register = () => (
+  <>
+    <h1>Register</h1>
+    <p>Not found</p>
+  </>
+);
+
+export default Register;
