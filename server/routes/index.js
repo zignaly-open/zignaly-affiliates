4df4adf3b6b1167c2bc24bda3f1a676b997529af
Alexander Mikhalchenko <alex@xfuturum.com>
2020-10-22 10:14:39 +0300

    API for codes
    (cherry picked from commit 74dfda24c983d1fd31d583d33abbc77a5c1a1223)

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,10 +3,12 @@ import dashboardRoutes from './dashboard';
 import uploadRoutes from './upload';
 import campaignRoutes from './campaign';
 import redirectRoutes from './redirect';
+import codeRoutes from './codes';
 import { isAuthenticated } from '../middleware/auth';
 
 export default app => {
   app.use('/api/v1/campaign', isAuthenticated(), campaignRoutes);
+  app.use('/api/v1/code', codeRoutes);
   app.use('/api/v1/user', userRoutes);
   app.use('/api/v1/dashboard', dashboardRoutes);
   app.use('/api/v1/upload', uploadRoutes);
