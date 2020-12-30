94733fe32d2bb4aab68d36fd9986be11e1884ffe
Alexander Mikhalchenko <alex@xfuturum.com>
2020-12-30 06:48:35 +0300

    Fixed duplicate keys
    (cherry picked from commit 8117389223bfc59ccb99c57ca9cee83e8e04a01f)

--- client/src/common/molecules/WallOfText.js
+++ client/src/common/molecules/WallOfText.js
@@ -7,13 +7,12 @@ const WallOfText = ({ title, text, id }) => (
   <Description id={id}>
     {!!title && <Title>{title}</Title>}
 
-    {text
+    {
+      text
       .split('\n')
       .filter(x => x)
-      .map((p, i) => (
-        // eslint-disable-next-line react/no-array-index-key
-        <p key={title + id + i}>{p}</p>
-      ))}
+      .map((p, i) => <p key={Math.random()}>{p}</p>)
+    }
   </Description>
 );
 
