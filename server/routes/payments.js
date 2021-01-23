4342953547b4809bb6f64563c6a1bb466581c4d8
Alexander Mikhalchenko <alex@xfuturum.com>
2021-01-24 01:23:01 +0300

    Review fixes

--- server/routes/payments.js
+++ server/routes/payments.js
@@ -5,15 +5,18 @@ import {
   payPayout,
   requestPayoutFromMerchantSide,
 } from '../controller/payments';
+import {isRole} from "../middleware/auth";
+import {USER_ROLES} from "../model/user";
 
 const router = express.Router();
 
 router.get('/', getPayments);
 router.post(
   '/merchant-payout/:campaign/:affiliate',
+  isRole(USER_ROLES.MERCHANT),
   requestPayoutFromMerchantSide,
 );
-router.post('/payout/:id', payPayout);
-router.post('/chain/dispute/:id', disputeChain);
+router.post('/payout/:id', isRole(USER_ROLES.MERCHANT), payPayout);
+router.post('/chain/dispute/:id', isRole(USER_ROLES.MERCHANT), disputeChain);
 
 export default router;
