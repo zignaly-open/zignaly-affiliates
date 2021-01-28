7a5316c1062f3541ec05e1adc268f4cbbce12176
Alexander Mikhalchenko <alex@xfuturum.com>
2021-01-28 03:51:37 +0300

    Linter

--- client/src/components/Payments/components/EnterTransactionId.js
+++ client/src/components/Payments/components/EnterTransactionId.js
@@ -3,12 +3,7 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogActions from '@material-ui/core/DialogActions';
 import PropTypes from 'prop-types';
-import React, {
-  useCallback,
-  useContext,
-  useMemo,
-  useState,
-} from 'react';
+import React, { useCallback, useContext, useMemo, useState } from 'react';
 import Dialog from '@material-ui/core/Dialog';
 import { Controller, useForm } from 'react-hook-form';
 import Button from '../../../common/atoms/Button';
