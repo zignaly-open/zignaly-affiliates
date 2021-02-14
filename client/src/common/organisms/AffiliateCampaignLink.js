4b9a1c9ee728c8e689faf59093ad0a07d12788cf
Alexander Mikhalchenko <alex@xfuturum.com>
2021-02-14 16:09:50 +0300

    No more link regeneration

--- client/src/common/organisms/AffiliateCampaignLink.js
+++ client/src/common/organisms/AffiliateCampaignLink.js
@@ -1,11 +1,10 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import useAsyncFn from 'react-use/lib/useAsyncFn';
 import Code from '../atoms/Code';
 import CopyButton from '../molecules/CopyButton';
 import Muted from '../atoms/Muted';
-import Loader from '../atoms/Loader';
 import { appContext } from '../../contexts/app';
 import Title from '../atoms/Title';
 import Button from '../atoms/Button';
@@ -19,26 +18,14 @@ const AffiliateCodeGenerator = ({
 }) => {
   const { api } = useContext(appContext);
   const { reloadCampaignSilently } = useContext(affiliateCampaignContext);
-  const [linkToShow, setLinkToShow] = useState(shortLink);
-  const [
-    { loading: generatingLink },
-    generateAnotherLink,
-  ] = useAsyncFn(async () => {
-    const { shortLink: newLink } = await api.post(
-      `campaign/marketplace/${_id}/new-link`,
-    );
-    setLinkToShow(newLink);
-  }, [_id, api]);
 
   const [{ loading: archiving }, archive] = useAsyncFn(
     () => api.post(`campaign/archive/${_id}`).then(reloadCampaignSilently),
     [_id],
   );
 
-  const url = process.env.REACT_APP_REDIRECT_BASE + linkToShow;
-  return generatingLink ? (
-    <Loader />
-  ) : (
+  const url = process.env.REACT_APP_REDIRECT_BASE + shortLink;
+  return (
     <CodeWrapper>
       <Title>Promoting link</Title>
       <Code big>
@@ -52,13 +39,6 @@ const AffiliateCodeGenerator = ({
         alertText="Link copied!"
       />
       <Muted block marginTop={15} small>
-        Don&apos;t like the link?{' '}
-        <Button link onClick={generateAnotherLink}>
-          Generate a different one
-        </Button>
-        . <br />
-        Referrals that come through the old link will not be counted.
-        <br />
         <div
           data-tootik-conf="multiline"
           data-tootik="You'll continue to receive rewards for affiliates converted through your links"
