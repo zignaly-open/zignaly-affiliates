0b94c5e2b391aeb74c1d46126f9c954688c36fe7
Alexander Mikhalchenko <alex@xfuturum.com>
2021-02-14 14:26:40 +0300

    Email and linter fixes

--- server/service/email.js
+++ server/service/email.js
@@ -5,6 +5,7 @@ import {
   SENDGRID_API_KEY,
   SENDGRID_CC_FOR_USER_EMAILS,
   SENDGRID_FROM_EMAIL,
+  SENDGRID_FROM_NAME,
 } from '../config';
 import { logError } from './logger';
 
@@ -14,7 +15,13 @@ client.setApiKey(SENDGRID_API_KEY);
 
 const send = payload => {
   if (ENVIRONMENT !== 'test') {
-    return client.send(payload);
+    return client.send({
+      from: {
+        name: SENDGRID_FROM_NAME,
+        email: SENDGRID_FROM_EMAIL,
+      },
+      ...payload,
+    });
   }
   return Promise.resolve();
 };
@@ -25,23 +32,30 @@ export const sendPasswordReset = async ({ email, resetToken }) => {
     // TODO: sendgrid templates
     await send({
       to: email,
-      from: SENDGRID_FROM_EMAIL,
       subject: 'Reset your password',
-      html: `
-        <h1>Reset your password</h1>
+      html: getMessageBody(
+        'Reset your password',
+        `
         <p>You are receiving this email because a request was made to reset the password for your Zignaly account (<a href='mailto:${email}' target='_blank' rel='noopener'>${email}</a>).</p>
         <p>If you did not ask to reset your password, then you can ignore this email and your password will not be changed.</p>
         <p><strong>The link below will remain active for 15 minutes and expires when visited, so please be prepared to set your password when you use it.</strong></p>
         <p><a href='${link}'>${link}</a></p>
-        <p/>
-        <p>The Zignaly Team</p>
-        <p><img src='https://zignaly.com/images/logo-w.png' alt='Zignaly' width='114' height='27' /></p>
     `,
+      ),
     });
   } catch (error) {
     logError(error.response.body);
   }
 };
+
+const getMessageBody = (title, body) => `
+    <h1>${title}</h1>
+    ${body}
+    <p/>
+    <p/>
+    <p>The Zignaly Team</p>
+    <p><img src='https://affiliate.zignaly.com/logo.svg' alt='Zignaly' width='136' height='35' /></p>
+`;
 
 const escape = string =>
   String(string)
@@ -59,18 +73,15 @@ export const sendEmailFromAnotherUser = async ({ email, text, emailFrom }) => {
     await send({
       to: email,
       bcc: SENDGRID_CC_FOR_USER_EMAILS,
-      from: SENDGRID_FROM_EMAIL,
       subject: 'Email from an affiliate',
-      html: `
-        <h1>Email from an affiliate</h1>
+      html: getMessageBody(
+        'Email from an affiliate',
+        `
         <p><a href="mailto:${emailFrom}">${emailFrom}</a> has sent you an email. Below is their message:</p>
         <p/>
         <p>${textPares.join('</p><p>')}</p>
-        <p/>
-        <p/>
-        <p>The Zignaly Team</p>
-        <p><img src='https://zignaly.com/images/logo-w.png' alt='Zignaly' width='114' height='27' /></p>
     `,
+      ),
     });
   } catch (error) {
     logError(error.response.body);
