5b2bb7037f4b06754a1ef4c700d85dd0143b061f
Christophe Wagner <chris.wagn22@gmail.com>
2021-10-04 17:47:52 +0200

    Update install hook for pm2

--- scripts/AfterInstallHook.sh
+++ scripts/AfterInstallHook.sh
@@ -2,5 +2,4 @@
 
 cd /var/www/zignaly-affiliates/server
 npm i
-# Assume it was already started with pm2 start npm --name "affiliates-server" -- start
-pm2 restart affiliates-server
+pm2 delete affiliates-server || : &&  pm2 start npm --name "affiliates-server" -- start
