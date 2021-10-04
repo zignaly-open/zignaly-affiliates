42752ce1e957535c4892aa24106af8b1c319d52a
Christophe Wagner <chris.wagn22@gmail.com>
2021-10-04 15:12:32 +0200

    Typo

--- scripts/AfterInstallHook.sh
+++ scripts/AfterInstallHook.sh
@@ -2,5 +2,5 @@
 
 cd /var/www/zignaly-affiliates/server
 npm i
-# Assume it was already start with pm2 start npm --name "affiliates-server" -- start
+# Assume it was already started with pm2 start npm --name "affiliates-server" -- start
 pm2 restart affiliates-server
