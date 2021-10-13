8c22ef95026a49330a506f74580cd52184d1251c
Christophe Wagner <chris.wagn22@gmail.com>
2021-10-13 21:04:46 +0200

    Debug deploy hook

--- scripts/AfterInstallHook.sh
+++ scripts/AfterInstallHook.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>/home/debug.log 2>&1
 cd /var/www/zignaly-affiliates/server
 # npm i
 # pm2 delete affiliates-server || : &&  pm2 start npm --name "affiliates-server" -- start
