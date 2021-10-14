da2bec2805e38912f66a791a49c96cc0a29548c6
Christophe Wagner <chris.wagn22@gmail.com>
2021-10-14 13:11:41 +0200

    Run pm2 as zignaly

--- scripts/AfterInstallHook.sh
+++ scripts/AfterInstallHook.sh
@@ -2,5 +2,6 @@
 
 cd /var/www/zignaly-affiliates/server
 npm i
-pm2 delete affiliates-server || : && NODE_ENV=production pm2 start npm --name "affiliates-server" -- start
 sudo chown -R zignaly:zignaly .
+sudo -u zignaly
+pm2 delete affiliates-server || : && NODE_ENV=production pm2 start npm --name "affiliates-server" -- start
